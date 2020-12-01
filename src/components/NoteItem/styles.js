316814456159ddb9758eb933bd620a7629656d9f
joaomarcuscardoso <rfcjoujou@gmail.com>
2020-12-01 19:07:18 -0300

    Projeto bloco de notas, foi feito a navegação entre telas, configurado redux, e parte visual

---
+++ src/components/NoteItem/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Box = styled.TouchableHighlight`
+  padding:15px;
+  border-style:solid;
+  border-bottom-color:#222;
+  border-bottom-width:1px;
+
+
+`;
+export const Title = styled.Text`
+  color:#fff;
+  font-size:17px;
+`;
