c72c8cb60478812a71c75b847f02f481622e60a1
joaomarcuscardoso <rfcjoujou@gmail.com>
2020-12-02 12:28:56 -0300

    Foi feito a parte de editar, deletar e adicionar

--- src/pages/EditNoteScreen/index.js
+++ src/pages/EditNoteScreen/index.js
@@ -1,13 +1,124 @@
-import React from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import { Alert } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigation, useRoute} from '@react-navigation/native';
+
 import {
-  Container, 
-  Texto
+  Container,
+  TitleInput,
+  BodyInput,
+  SaveButton,
+  SaveButtonImage,
+  CloseButton,
+  CloseButtonImage,
+  DeleteButton,
+  DeleteButtonText
 } from './styles';
+import { SwipeListView } from 'react-native-swipe-list-view';
 
 export default () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const dispatch = useDispatch();
+  const list = useSelector(state => state.notes.list);
+
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [ status, setStatus ] = useState('new');
+
+  useEffect(()=> {
+    if(route.params?.key != undefined && list[route.params.key]) {
+      setStatus('edit');
+      setTitle(list[route.params.key].title);
+      setBody(list[route.params.key].body);
+    }
+  }, []);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: status == "new"? "Nova Anotação": "Editar anotação",
+      headerLeft: () => (
+        <CloseButton underlayColor="transparent" onPress={handleCloseButton} >
+          <CloseButtonImage source={require("../../assets/close.png")} />
+        </CloseButton>
+      ),
+      headerRight: () => (
+        <SaveButton underlayColor="transparent" onPress={handleSaveButton}>
+          <SaveButtonImage source={require("../../assets/save.png")} />
+        </SaveButton>
+      )
+    });
+  }, [status, title, body]); 
+  
+  const handleCloseButton = () => {
+    navigation.goBack();
+  }
+
+  const handleSaveButton = () => {
+    if(title  != "" && body != "") {
+      if(status == "edit") {
+        dispatch({
+          type: "EDIT_NOTE",
+          payload: {
+            key: route.params.key,
+            title,
+            body
+          }
+        })
+      } else {
+        dispatch({
+          type:"ADD_NOTE",
+          payload:{
+            title,
+            body
+          }
+
+        });
+      }
+
+      navigation.goBack();
+    } else {
+      Alert.alert("Atenção", "Preencha titulo e corpo.");
+    }
+  }
+
+  const handleDeleteNoteButton = () => {
+    dispatch({
+      type:"DEL_NOTE",
+      payload: {
+        key:route.params.key
+      }
+    });
+
+    navigation.goBack();
+  }
+
   return(
     <Container>
-      <Texto>Tela de Editar</Texto>
+
+
+        <TitleInput 
+          value={title}
+          onChangeText={t=>setTitle(t)}
+          placeholder="Digite o titulo da anotação"
+          placeholderTextColor="#ccc"
+          autoFocus={true}
+        />
+        <BodyInput 
+          value={body}
+          onChangeText={t=>setBody(t)}
+          placeholder="Digite sua anotação"
+          placeholderTextColor="#ccc"
+          multiline={true}
+          textAlignVertical="top"
+        />
+        {status === "edit" && 
+          <DeleteButton underlayColor="#FF0000" onPress={handleDeleteNoteButton}>
+            <DeleteButtonText>Excluir Anotação</DeleteButtonText>
+          </DeleteButton>
+        }
+   
     </Container>
   );
 }
--- src/pages/EditNoteScreen/styles.js
+++ src/pages/EditNoteScreen/styles.js
@@ -1,5 +1,49 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+flex:1;
+background-color:#333;
+`;
+export const TitleInput = styled.TextInput`
+font-size:20px;
+font-weight:bold;
+padding:15px;
+color:#fff;
+`;
 
-export const Texto = styled.Text``;
+export const BodyInput = styled.TextInput`
+flex:1;
+padding:15px;
+font-size:15px;
+color:#fff;
+
+`;
+export const SaveButton = styled.TouchableHighlight`
+margin-right:15px;
+
+`;
+export const SaveButtonImage = styled.Image`
+width:22px;
+height:22px;
+
+`;
+export const CloseButton = styled.TouchableHighlight`
+margin-left:15px;
+align-items:center;
+`;
+export const CloseButtonImage= styled.Image`
+
+width:18px;
+height:18px;
+`;
+
+export const DeleteButton = styled.TouchableHighlight`
+height:40px;
+background-color:#FF3333;
+justify-content:center;
+align-items:center;
+`;
+export const DeleteButtonText = styled.Text`
+font-size:14px;
+color:#fff;
+`;
